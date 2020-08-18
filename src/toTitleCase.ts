d79d03f9b7677617c148f76718a7b6a09fc73c44
Robin Blomberg <robin.blomberg@hiq.se>
2020-08-18 08:52:21 +0200

    toTitleCase: Cast to string

--- src/toTitleCase.ts
+++ src/toTitleCase.ts
@@ -1,6 +1,6 @@
 import words from './toWords'
 
-export default (string: string) => {
+export default (string: unknown) => {
   return words(string)
     .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
     .join(' ')
--- test/toTitleCase.ts
+++ test/toTitleCase.ts
@@ -5,5 +5,9 @@ strictEqual(
   toTitleCase(' foo bar, bAz.  56  '),
   'Foo Bar Baz 56'
 )
+strictEqual(
+  toTitleCase(36),
+  '36'
+)
 
 console.log('\u001b[32m✓ Test "toTitleCase" passed.\u001b[39m')
